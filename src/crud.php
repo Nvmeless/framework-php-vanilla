0ba0eb3f7d880f4df92afdb814ce8ce143e62f54
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2023-10-06 15:11:57 +0200

    debut du form

--- src/crud.php
+++ src/crud.php
@@ -1,28 +1,121 @@
 <?php 
 require_once "./src/dbConnect.php";
 
+function queryBuilder($method, $table, ...$payload){
+    $query ="";
+    switch ($method) {
+        case 'c':
+            $query .= "INSERT INTO ";
+            break;
+        case 'r':
+            $query .= "SELECT * FROM ";
+            break;
+        case 'u':
+            $query .= "UPDATE ";
+            break;
+        case 'd':
+            $query .= "DELETE ";
+            break;
+        default:
+           
+            die("ERROR : Prepared query method not defined");
+            break;
+    }
+
+    $query .= '`'.  htmlspecialchars($table) . '` ';
+    if($method ==='u'){
+        $query .= "SET ";
 
 
-//fonction getAll
-$statement = $connection->query("SELECT * FROM contacts WHERE 1");
-$data = $statement->fetchAll(PDO::FETCH_ASSOC);
-//fonction getById
-$statement = $connection->query("SELECT * FROM contacts WHERE `name` =  'Delaistre' AND `surname` = '".htmlspecialchars( $_GET["surname"])."'");
-$data = $statement->fetchAll(PDO::FETCH_ASSOC);
-//fonction create 
-$statement = $connection->prepare("INSERT INTO `contacts` (`name`, `surname`, `status`) VALUES (?, ?, 'online') ");
-$statement->bindParam(1,$_GET["name"]);
-$statement->bindParam(2,$_GET["surname"]);
-$statement->execute();
+    }
+    if($method ==="c"){
+        $columnParse  = '(';
+        $valueParse  = '(';
+        foreach ($payload as $index => $column) {
+            foreach ($column as $key => $value) {
+                if(is_string($value)){
+                    $value = "\"" . $value. "\"";
+                }
+                $columnParse .= "`" . $key . "`"; 
+                 if(!(count($payload) == ($index + 1 ))){
+                $columnParse .= ", ";
+            }
+            }
 
-//fonction delete
-$statement = $connection->prepare("DELETE FROM `contacts` WHERE id = ?");
-$id = 3;
-$statement->bindParam(1, $id);
-$statement->execute();
-//fonction update
+        }
+        $columnParse.= ")";
+             foreach ($payload as $index => $column) {
+            foreach ($column as $key => $value) {
+                if(is_string($value)){
+                    $value = "\"" . $value. "\"";
+                }
+                $valueParse .= $value ; 
+                 if(!(count($payload) == ($index + 1 ))){
+                $valueParse .= ", ";
+            }
+            }
 
-$statement = $connection->prepare("UPDATE `contacts` SET `status` = 'offline' WHERE id = ?");
-$id = 2;
-$statement->bindParam(1, $id);
-$statement->execute();
+        }
+        $valueParse.= ")";
+        $query .= $columnParse . " VALUES " . $valueParse;
+    }
+    if($method ==='u'){
+        foreach ($payload as $index => $filter) {
+            foreach ($filter as $key => $value) {
+                if($key !== "id"){
+                    if(is_string($value)){
+                        $value = "\"" . $value. "\"";
+                    }
+                    
+                    $query .= "`" . $key . "` = ". $value .' ' ; 
+                    
+                    if(!(count($payload) == ($index + 2 ))){
+                        $query .= ", ";
+                    }
+                }
+            }
+
+        }
+    }
+    if($method !=='c' && $method !== "u" && count($payload)){
+        $query .= "WHERE ";
+        foreach ($payload as $index => $filter) {
+            foreach ($filter as $key => $value) {
+                if(is_string($value)){
+                    $value = "\"" . $value. "\"";
+                }
+                $query .= "`" . $key . "` = ". $value . " AND "; 
+            }
+            if(count($payload) == ($index + 1 ) && $method !=='r'){
+                $query .= "1";
+            } else if(count($payload) == ($index + 1 )) {
+                $query .= '`status` = "online"';
+            }
+        }
+    } else if($method === "u"){
+        $idFound = false;
+        foreach ($payload as $index => $filter) {
+            foreach ($filter as $key => $value) {
+                if($key === "id"){
+                    $idFound = true;
+                
+                    $query .= "WHERE ";
+                    $query .= "`" . $key . "` = ". $value;
+                } 
+            }
+        }
+        if(!$idFound){
+            die("ERROR : Not id to update");
+        }
+    }
+    
+   return $query;
+
+} 
+// dd(queryBuilder("c", "voiture", ["modele" =>"Ferrari"], ["couleur" => "rouge" ], ["test" => "taste"]));
+// dd(queryBuilder("r", "contacts",  ["name" => "Delaistre" ]));
+// dd(queryBuilder("u", "voiture", ["modele" => "Ferrari" ], ["couleur" => "rouge" ], ["id" => 2]));
+// dd(queryBuilder("d", "voiture", ["modele" => "Ferrari" ], ["couleur" => "rouge" ]));
+
+
+
--- templates/layouts/html.layout.php
+++ templates/layouts/html.layout.php
@@ -2,4 +2,32 @@
 include_once './templates/includes/html_header.inc.php';
 fromInc("menu");
 echo $pageContent['html'];
+?>
+<form action="/" method="post">
+  <ul>
+    <li>
+      <label for="name">Nom&nbsp;:</label>
+      <input type="text" id="name" name="name" />
+    </li>
+    <li>
+      <label for="surname">prenom&nbsp;:</label>
+      <input type="text" id="surname" name="surname" />
+    </li>
+
+  </ul>
+</form>
+<ul>
+    <?php 
+    foreach ($pageContent["data"]['contacts'] as $key => $value) {
+        ?>
+        <li>
+            <h4><?= $value["surname"]?></h4>
+            <h5><?= $value["name"]?></h5>
+        </li>
+        <?php
+    }
+    ?>
+</ul>
+
+<?= 
 include_once './templates/includes/html_footer.inc.php';
