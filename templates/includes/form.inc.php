88449d48f5c601f8a48dff3ff3eda33f7e0823d1
Nvmeless <alexandre.quilan.delaistre@gmail.com>
2023-10-06 16:31:25 +0200

    Done

--- templates/includes/form.inc.php
+++ templates/includes/form.inc.php
@@ -2,17 +2,22 @@
 require './src/dbConnect.php';
 require './configs/global.php';
 ?>
-<form action="/" method="post">
+<form action="#" method="post">
   <ul>
     <li>
-      <label for="name">Nom&nbsp;:</label>
+      <label for="name">Nom du pelo&nbsp;:</label>
       <input type="text" id="name" name="name" />
     </li>
     <li>
-      <label for="surname">prenom&nbsp;:</label>
+      <label for="surname">Prénom du pelo&nbsp;:</label>
       <input type="text" id="surname" name="surname" />
     </li>
 
   </ul>
+  <input type="submit" value="Ajouter un Pelo" />
 </form>
 
+<?php 
+if(isset($_POST['name']) && isset($_POST['surname'])){
+    $connection->query(queryBuilder('c', 'contacts', ['name' => $_POST['name']], ['surname' => $_POST['surname']]));
+}
